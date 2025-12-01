1eb958d0ea018c2b7877e2bb633098f4d65da129
cucurumbia <iksaruk@gmail.com>
2025-12-01 15:20:08 +0300

    что-то

--- book_store/book_store/catalog.xaml.cs
+++ book_store/book_store/catalog.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace book_store
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для catalog.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class catalog : Window
     {
-        public MainWindow()
+        public catalog()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
